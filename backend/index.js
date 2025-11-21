7aa8fa8e504adc625cd92af60f385905dd988352
đồ án <thanhrmos04/2gmail.com>
2025-11-21 15:59:24 +0700

    Applied changes and fixes to package.json

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,30 @@ import 'dotenv/config'; // Cách 1: Import và tự động chạy config
 import router from './routes/index.js';
 import connectMDB from './connect.js';
 import cors from 'cors';
+import path from "path";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 //middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+if(process.env.NODE_ENV === 'production'){
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
+
 
 
 // Routes
 // Lưu ý: Bạn đang import 'router' từ './routes/index.js' và gọi nó như một function.
 // Đảm bảo file './routes/index.js' của bạn được thiết kế để nhận 'app' làm đối số.
 router(app);
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+  app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+  });
+}
 
 //kết nối với database
 const uri = process.env.MONGO_URI || null;
