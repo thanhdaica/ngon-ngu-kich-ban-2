ae08acc3f46b00cd78d597dc90a43fe419ed0dc1
đồ án <thanhrmos04/2gmail.com>
2025-11-29 14:58:45 +0700

    đã thêm email ạ

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,5 @@
 import routerBooks from "./books.js";
 import routerCategorys from "./categorys.js";
-import routerUserbooks from "./userbooks.js";
 import routerUsers from "./users.js";
 import routerCarts from "./carts.js";
 import routerOrders from "./orders.js";
@@ -9,7 +8,6 @@ import routerPayment from "./payments.js";
 export default function router (app){
     app.use('/api/book', routerBooks);
     app.use('/api/category', routerCategorys);
-    app.use('/api/userbook', routerUserbooks);
     app.use('/api/user', routerUsers);
     app.use('/api/cart', routerCarts);
     app.use('/api/order', routerOrders);
